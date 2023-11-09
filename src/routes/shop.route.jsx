175aab36e9fb71736aa4453467668a8d37038018
Raghib <raghib108@gmail.com>
2023-11-09 13:24:24 +0500

    Added shop component

--- src/routes/shop.route.jsx
+++ src/routes/shop.route.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import ProductCard from "../components/product-card";
+import { ProductsContext } from "../contexts/shop.context";
+import { useContext } from "react";
+import "./shop.styles.scss";
 
 const Shop = () => {
-  return <div>Shop Page</div>;
+  const { products } = useContext(ProductsContext);
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default Shop;
