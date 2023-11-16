b266d3dbe65eca18fef2edb3c84aea10bf76ced2
Raghib <raghib108@gmail.com>
2023-11-16 15:30:50 +0500

    Updated Navbar styles to styled components

--- src/routes/navbar.route.jsx
+++ src/routes/navbar.route.jsx
@@ -1,41 +1,43 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { UserContext } from "../contexts/user.context";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { signOutUser } from "../utils/firebase.utils";
 import Logo from "../assets/crown.svg";
 import CartIcon from "../components/cart-icon";
 import CartDropDown from "../components/cart-dropdown";
 import { CartContext } from "../contexts/cart.context";
 
-import "./navbar.styles.scss";
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from "./navbar.styles";
 
 const NavBar = () => {
   const { currentUser } = useContext(UserContext);
-  const { isCartOpen, setCartOpen } = useContext(CartContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <img src={Logo} className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            <div>Shop</div>
-          </Link>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
+
           {currentUser ? (
-            <Link className="nav-link" onClick={signOutUser}>
-              <div>Sign Out</div>
-            </Link>
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              <div>Sign In</div>
-            </Link>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropDown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </>
   );
