30fe86418f0c117fb02ef1053bcb48ad91934adb
Raghib <raghib108@gmail.com>
2023-11-24 22:12:19 +0500

    _

--- src/components/form-input.jsx
+++ src/components/form-input.jsx
@@ -4,6 +4,7 @@ export const FormInput = ({ label, ...otherProps }) => {
   return (
     <Group>
       <Input {...otherProps} />
+
       {label && (
         <FormInputLabel shrink={otherProps.value.length}>
           {label}
